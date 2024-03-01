9c7789865df57e8ad8ac0159b1dce3ede8d3435a
solangeihirwe03 <solangeduhimbazeihirwe@gmail.com>
2024-03-01 14:19:29 +0200

    new commit

--- calculator.js
+++ calculator.js
@@ -1,15 +1,7 @@
  
 function calculate() {
-  try{
     var result = eval(document.form[0].display.value);
     if (result=== infinity || result=== -infinity){
-      throw "infinity"
-    }
-    else{
-      document.forms[0].display.value = result;
-    }
-  }
-    catch(error){
-      document.forms[0].display.value = "error";
+      result = "error"
     }
   }
